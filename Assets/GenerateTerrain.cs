27653d2f39c13dd7f9d562bcea802419131687d4
maxmmyron <maxmmyron@gmail.com>
2021-11-16 14:33:10 -0500

    updated chunk rendering

--- Assets/GenerateTerrain.cs
+++ Assets/GenerateTerrain.cs
@@ -8,15 +8,12 @@ public class GenerateTerrain : MonoBehaviour
     private GameObject Chunk;
 
     [SerializeField]
-    private int terrainSize = 2;
-
-    [SerializeField]
-    private int offset = 1;
-
-    private int oldOffset;
+    private int terrainSize = 8;
 
     private void Generate()
     {
+        int seed = (int)Random.Range(0.0f, 100.0f);
+
         for (int x = 0; x < terrainSize; x++)
         {
             for (int y = 0; y < terrainSize; y++)
@@ -25,7 +22,7 @@ public class GenerateTerrain : MonoBehaviour
                 {
                     GameObject chunkPrefab = Instantiate(Chunk) as GameObject;
                     chunkPrefab.transform.position = new Vector3(x, y, z);
-                    chunkPrefab.GetComponent<GenerateChunkTerrain>().offset = offset;
+                    chunkPrefab.GetComponent<GenerateChunkTerrain>().seed = seed;
                 }
             }
         }
@@ -33,19 +30,6 @@ public class GenerateTerrain : MonoBehaviour
 
     private void Start()
     {
-        oldOffset = offset;
         Generate();
-    }
-
-    
-    private void Update()
-    {
-        if (oldOffset != offset)
-        {
-            for (int i = 1; i < gameObject.transform.childCount; i++)
-                GameObject.Destroy(gameObject.transform.GetChild(i).gameObject);
-            Generate();
-            oldOffset = offset;
-        }
     }
 }
